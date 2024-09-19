1d34cbd02b21cf3598d516d64e0f7ffc3521fcd4
Evan Alif Widhyatma <evanalifwidhyatma@gmail.com>
2024-09-19 08:16:35 +0700

    initial commit ESP32_LCD_DISPLAY

--- Iot_Firmware/ESP32_SENDER/src/rahasia.h
+++ Iot_Firmware/ESP32_SENDER/src/rahasia.h
@@ -3,5 +3,5 @@
 #define RAHASIA_SSID "server"		//Isikan dengan SSID
 #define RAHASIA_PASS "jeris6467"	//Isikan dengan Password
 
-#define RAHASIA_CH_ID 1		//ISikan dengan Channel ID	
-#define RAHASIA_WRITE_APIKEY "E4LDA72WDCG5T7QF"  //Isikan dengan Write API Key
+#define RAHASIA_CH_ID 2		//ISikan dengan Channel ID	
+#define RAHASIA_WRITE_APIKEY "FCFMA0BDB3NACBXE"  //Isikan dengan Write API Key
