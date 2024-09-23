a8e673777c9a42c57f3f60621481c16c65f4f113
Evan Alif Widhyatma <evanalifwidhyatma@gmail.com>
2024-09-23 23:46:52 +0700

    modified:   .gitignore 	new file:   Iot_Firmware/LoRa-Gateway/src/rahasia.h

---
+++ Iot_Firmware/LoRa-Gateway/src/rahasia.h
@@ -0,0 +1,27 @@
+//Gunakan untuk menyimpan informasi rahasia seperti SSID dan Password
+
+//Wifi 1
+#define RAHASIA_SSID1 "server"		//Isikan dengan SSID1
+#define RAHASIA_PASS1 "jeris6467"	//Isikan dengan Password1
+
+//Wifi 2
+#define RAHASIA_SSID2 "Jerukagung Seismologi"		//Isikan dengan SSID2
+#define RAHASIA_PASS2 "riset1234"	//Isikan 
+
+//Credentials Client
+//WEATHERCLOUD
+#define DEVICEID    "e2013519a7853ac0"
+#define DEVICEAPI   "eff661ec5f6725c5d95fec8fcf9d92f8"
+
+//WINDY
+#define WINDY_API ""
+
+//THINGSPEAK
+#define CH_ID 2		//ISikan dengan Channel ID	
+#define WRITE_APIKEY "FCFMA0BDB3NACBXE"  //Isikan dengan Write API Key
+
+//FIREBASE
+#define FIREBASE_HOST "staklimjerukagung-default-rtdb.asia-southeast1.firebasedatabase.app"	//Isikan dengan Firebase Host
+#define FIREBASE_AUTH "AIzaSyDalcCwwOthPMjC3umkpQECqlQQj699FTY"	//Isikan dengan Firebase Auth
+#define FIREBASE_USER "firebase@gmail.com"
+#define FIREBASE_PASS "litbang1234"
