139060356571b655feb5dbd4788ff642b5d85564
Evan Alif Widhyatma <evanalifwidhyatma@gmail.com>
2024-09-28 10:41:55 +0700

    Update Firebase RTDB and Firestore hosts in rahasia.h

--- Iot_Firmware/ESP32_LCD_DISPLAY (Testing)/src/rahasia.h
+++ Iot_Firmware/ESP32_LCD_DISPLAY (Testing)/src/rahasia.h
@@ -21,7 +21,8 @@
 #define WRITE_APIKEY "FCFMA0BDB3NACBXE"  //Isikan dengan Write API Key
 
 //FIREBASE
-#define FIREBASE_HOST "staklimjerukagung-default-rtdb.asia-southeast1.firebasedatabase.app"	//Isikan dengan Firebase Host
-#define FIREBASE_AUTH "AIzaSyDalcCwwOthPMjC3umkpQECqlQQj699FTY"	//Isikan dengan Firebase Auth
+#define FIREBASE_RTDB_HOST "https://database-sensor-iklim-litbang-default-rtdb.asia-southeast1.firebasedatabase.app/"	//Isikan dengan Firebase Host
+#define FIREBASE_FIRESTORE_HOST ""    //Isikan dengan Firestore Host
+#define FIREBASE_AUTH "AIzaSyDPoeHnDs1E4BxAMDoLEub2uE9q6H_YDw4"	//Isikan dengan Firebase Auth
 #define FIREBASE_USER "firebase@gmail.com"
 #define FIREBASE_PASS "litbang1234"
