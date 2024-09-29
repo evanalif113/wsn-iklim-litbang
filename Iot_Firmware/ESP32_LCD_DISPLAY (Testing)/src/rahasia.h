0dee17bca51ed51790ea239c6cfcfe88206dc6ab
Evan Alif Widhyatma <evanalifwidhyatma@gmail.com>
2024-09-29 18:15:13 +0700

    Refactor Firebase credentials in rahasia.h

--- Iot_Firmware/ESP32_LCD_DISPLAY (Testing)/src/rahasia.h
+++ Iot_Firmware/ESP32_LCD_DISPLAY (Testing)/src/rahasia.h
@@ -24,5 +24,5 @@
 #define FIREBASE_RTDB_HOST "https://database-sensor-iklim-litbang-default-rtdb.asia-southeast1.firebasedatabase.app/"	//Isikan dengan Firebase Host
 #define FIREBASE_FIRESTORE_HOST ""    //Isikan dengan Firestore Host
 #define FIREBASE_AUTH "AIzaSyDPoeHnDs1E4BxAMDoLEub2uE9q6H_YDw4"	//Isikan dengan Firebase Auth
-#define FIREBASE_USER "firebase@gmail.com"
-#define FIREBASE_PASS "litbang1234"
+#define FIREBASE_USER "firebase@gmail.com" //Isikan dengan Firebase User Email
+#define FIREBASE_PASS "litbang1234" //Isikan dengan Firebase User Password
