fc4837884578770f1d01c8bddbd026092dfdc0b5
evanalif113 <evanalifwidhyatma@gmail.com>
2024-11-07 23:11:34 +0700

    Update Firebase library version in Front_End/package.json

---
+++ Front_End/public/scripts/config.js
@@ -0,0 +1,22 @@
+// fireconfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+
+// Konfigurasi Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDPoeHnDs1E4BxAMDoLEub2uE9q6H_YDw4",
+    authDomain: "database-sensor-iklim-litbang.firebaseapp.com",
+    databaseURL: "https://database-sensor-iklim-litbang-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "database-sensor-iklim-litbang",
+    storageBucket: "database-sensor-iklim-litbang.appspot.com",
+    messagingSenderId: "849206798206",
+    appId: "1:849206798206:web:7fe6fe938389658302752f",
+    measurementId: "G-GH86DQR6NJ"
+  };
+
+// Inisialisasi Firebase App
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export {analytics};
+export default app;  // Menggunakan default export untuk app
