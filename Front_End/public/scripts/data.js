74ceafa979e29c8e89a4ac9c67546f80d8b248b2
evanalif113 <evanalifwidhyatma@gmail.com>
2024-11-08 16:56:42 +0700

    Refactor data.html to fix missing table header

--- Front_End/public/scripts/data.js
+++ Front_End/public/scripts/data.js
@@ -21,10 +21,13 @@ function loadWeatherData() {
                 const data = childSnapshot.val();
                 const timeFormatted = new Date(data.timestamp * 1000)
                                         .toLocaleTimeString([], { 
+                                            year: 'numeric', 
+                                            month: '2-digit', 
+                                            day: '2-digit', 
                                             hour: '2-digit', 
                                             minute: '2-digit', 
                                             second: '2-digit', 
-                                            hour12: false 
+                                            hour12: false
                                         });
                 dataArray.push({
                     date: timeFormatted,
